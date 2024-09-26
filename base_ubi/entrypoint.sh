dc8dd404d108fe6a86938427c4f4db6244a8d118
Gabriel Iuhasz <iuhasz.gabriel@e-uvt.ro>
2024-09-26 14:03:33 +0300

    Added check for CONDA_ENVS_PATH, if not set create. Allows for devfile setting of default path.

--- base_ubi/entrypoint.sh
+++ base_ubi/entrypoint.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+
+# add .condarc with alternate env path
+
+# check if .condarc exists in $HOME
+#if [ -f $HOME/.condarc ]; then
+#  echo "File .condarc exists in $HOME"
+#  echo "Contents of .condarc:"
+#  cat $HOME/.condarc
+#else
+#  echo "File .condarc does not exist in $HOME creating it"
+#  echo "envs_dirs:" > $HOME/.condarc
+#fi
+
+# check if envvar set if not set it
+if [ -z "$CONDA_ENVS_PATH" ]; then
+  echo "CONDA_ENVS_PATH not set"
+  export CONDA_ENVS_PATH=$PROJECT_SOURCE/envs
+  echo "Setting CONDA_ENVS_PATH to $CONDA_ENVS_PATH"
+else
+  echo "CONDA_ENVS_PATH is set to $CONDA_ENVS_PATH"
+fi
+
 whoami
 which python
 echo $HOME
